d50cc474f125c8001ce7e6720ebd1051e62c0823
DH-O <tedoh94@gmail.com>
2023-11-09 16:42:56 +0900

    erase unusing arguments

--- MADDPG/utility/arguments.py
+++ MADDPG/utility/arguments.py
@@ -9,11 +9,11 @@ def parse_args():
     parser = argparse.ArgumentParser("reinforcement learning experiments for multiagent environments")
 
     # environment
-    parser.add_argument("--scenario_name", type=str, default="simple_adversary", help="name of the scenario script")
+    parser.add_argument("--scenario_name", type=str, default="simple_spread", help="name of the scenario script")
     parser.add_argument("--start_time", type=str, default=time_now, help="the time when start the game")
     parser.add_argument("--per_episode_max_len", type=int, default=1500, help="maximum episode length")
     parser.add_argument("--max_episode", type=int, default=150000, help="maximum episode length")
-    parser.add_argument("--num-adversaries", type=int, default=1, help="number of adversaries")
+    parser.add_argument("--num-adversaries", type=int, default=0, help="number of adversaries")
 
     # core training parameters
     parser.add_argument("--device", default=device, help="torch device ")
@@ -21,8 +21,8 @@ def parse_args():
     parser.add_argument("--max_grad_norm", type=float, default=0.5, help="max gradient norm for clip")
     parser.add_argument("--learning_fre", type=int, default=100, help="learning frequency")
     parser.add_argument("--tao", type=int, default=0.01, help="how depth we exchange the par of the nn")
-    parser.add_argument("--lr_a", type=float, default=1e-4, help="learning rate for adam optimizer")
-    parser.add_argument("--lr_c", type=float, default=1e-4, help="learning rate for adam optimizer")
+    parser.add_argument("--lr_a", type=float, default=1e-3, help="learning rate for adam optimizer")
+    parser.add_argument("--lr_c", type=float, default=1e-3, help="learning rate for adam optimizer")
     parser.add_argument("--gamma", type=float, default=0.999, help="discount factor")
     parser.add_argument("--batch_size", type=int, default=1024, help="number of episodes to optimize at the same time")
     parser.add_argument("--memory_size", type=int, default=1e6, help="number of data stored in the memory")
@@ -38,13 +38,4 @@ def parse_args():
     parser.add_argument("--old_model_name", type=str, default="models/1911_122134_20000/", \
             help="directory in which training state and model are loaded")
 
-    # evaluation
-    parser.add_argument("--restore", action="store_true", default=False)
-    parser.add_argument("--display", action="store_true", default=False)
-    parser.add_argument("--benchmark", action="store_true", default=False)
-    parser.add_argument("--benchmark-iters", type=int, default=100000, help="number of iterations run for benchmarking")
-    parser.add_argument("--benchmark-dir", type=str, default="./benchmark_files/", \
-            help="directory where benchmark data is saved")
-    parser.add_argument("--plots-dir", type=str, default="./learning_curves/", \
-            help="directory where plot data is saved")
     return parser.parse_args()
